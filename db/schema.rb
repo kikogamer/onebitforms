ad8dcbfe77e254b21967a35aac7cd1f346ace1db
Ronaldo <kikogamer@gmail.com>
2019-03-14 23:27:39 -0300

    allow create a optional question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190214020227) do
+ActiveRecord::Schema.define(version: 20190313014803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20190214020227) do
     t.integer  "form_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "required"
     t.index ["form_id"], name: "index_questions_on_form_id", using: :btree
   end
 
